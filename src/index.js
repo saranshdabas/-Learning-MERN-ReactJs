6458ef0eccd878606f458b20133a4bcb9fa5bb0d
Apple <sdjee2015@gmail.com>
2020-12-30 18:45:22 +0530

    Nested components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//JSX rules
-// Always close all the tags even those who don't have one like img, input
-// Use a () for return just to work with prettier else it will add ; after return
-// Always return only a single element, if you have multiple enclose them in div or React.fragment
-// Use camelCase for attributes like onClick in place of onclick
-// Use className in place of class
-
+//Nested componenets
 function Greeting() {
-  return <h1>Hi, Hello World!</h1>;
+  return (
+    <div>
+      <Heading />
+      <Message />
+    </div>
+  );
 }
+
+//Implicit arrow fn return
+const Heading = () => <h1>Heading of the century</h1>;
+
+//Explicit return
+const Message = () => {
+  return <h4>Hello world!</h4>;
+};
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
