c3ab629bc783f2441945cd8fcebc62bb7b090842
Apple <sdjee2015@gmail.com>
2020-12-31 19:21:46 +0530

    Key props

--- src/index.js
+++ src/index.js
@@ -6,25 +6,35 @@ import "./index.css";
 
 const bookList = [
   {
+    id: 1,
     title: "Ikigai: The Japanese secret to a long and happy life",
     author: "Héctor García",
     img:
       "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 2,
     title: "The Alchemist",
     author: "Paulo Coelho",
     img:
       "https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg",
   },
+  {
+    id: 3,
+    title: "The Theory of Everything",
+    author: "Stephen Hawking",
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51oHUvYzbsL._AC_UL200_SR200,200_.jpg",
+  },
 ];
 
 //Using map to return a list of books (JSX can render a list of objects)
+//Adding id as key prop in order for react to work with virtual dom as expected.
 function Greeting() {
   return (
     <section className="booklist">
       {bookList.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
