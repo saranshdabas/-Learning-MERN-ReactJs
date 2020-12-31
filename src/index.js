d8e962d26ee66e9f7045eb5ab63a2c028710d947
Apple <sdjee2015@gmail.com>
2020-12-31 12:17:57 +0530

    Inline CSS (JSX CSS)

--- src/index.js
+++ src/index.js
@@ -33,7 +33,11 @@ const Image = () => (
   <img src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" />
 );
 
-const Author = () => <h6>Héctor García</h6>;
+const Author = () => (
+  <h6 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Héctor García
+  </h6>
+);
 
 const Title = () => (
   <h5>Ikigai: The Japanese secret to a long and happy life </h5>
