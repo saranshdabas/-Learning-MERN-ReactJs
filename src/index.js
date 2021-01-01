41100013a976106ebd78a136330ce64625011db5
Apple <sdjee2015@gmail.com>
2021-01-01 11:10:42 +0530

    Event handling in JSX

--- src/index.js
+++ src/index.js
@@ -41,19 +41,34 @@ function Greeting() {
   );
 }
 
-//Passing props as parametres
+//Events can be handled inline or externally usinf reference
+//In case of reference and we want to pass an arugument we cannot use (), as it will be called on starting
+//We will need to use arrow fn
 const Book = (props) => {
-  //Props now contain spreaded properties(not book object) and hence we can destructure props itself.
   const { img, title, author } = props;
+  const handleClickEvent = () => {
+    console.log("Button was clicked");
+  };
+  const handleClickEventArgs = (author) => {
+    console.log(`Button of ${author} was clicked`);
+  };
   return (
     <article className="book">
-      <img src={img} />
+      <img onClick={() => console.log("Image was clicked")} src={img} />
       <h5> {title} </h5>
       <h6
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
         {author.toUpperCase()}
       </h6>
+      <button onClick={handleClickEvent}>without arg</button>
+      <button
+        onClick={() => {
+          handleClickEventArgs(author);
+        }}
+      >
+        with arg
+      </button>
     </article>
   );
 };
